e2084b45107297fee8135a61df7955b16e9bde69
Abubakr <thefox5000@gmail.com>
2024-04-21 11:21:43 +0300

    :gear: My first c# class

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class clsPerson
+{
+    public string firstName;
+    public string lastName;
+
+    public string fullName ()
+    {
+        return firstName + " " + lastName;
+    }
+
+    public void writeFullName ()
+    {
+        Console.WriteLine(fullName());
+    }
+}
+
+
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
+
+
